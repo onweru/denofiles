41b2e6449e9ee71adb9727d69e8d59ef861c6f8e
weru <onewesh@gmail.com>
2020-07-07 00:46:24 +0300

    save work

---
+++ README.md
@@ -0,0 +1,14 @@
+# Create files using Deno JS: 
+
+## A crude  example
+
+1. Create a json file and populate it with some information
+2. For more practical purposes, send a `GET` to an API that returns json
+3. Depending on the kind of info you want to save to different files, create your variables
+4. Use template literals to format your string as you'ld like
+
+> Once you're satisfied with the format, proceed
+
+To generate files from your json file. Run the following:
+
+`deno run --allow-read --allow-write index.ts`
---
+++ index.ts
@@ -0,0 +1,49 @@
+// read json file
+const decoder = new TextDecoder("utf-8");
+const rawPosts = await Deno.readFile("./posts.json");
+let myPosts = decoder.decode(rawPosts); 
+
+// parse the json object and loop through it
+JSON.parse(myPosts).forEach(function(post: any, index: Number){
+  const date = post.published;
+  const postSlug = date && date != "NULL"  ? date.substring(0,10) : "2014-07-04";
+  const permalink = post.permalink ? post.permalink : `${index}`;
+  let modified = post.modified;
+  modified = modified && modified != "NULL" ? modified : "2020-07-05";
+  const title = post.title && post.title.trim().length > 1 && post.title != "NULL" ? post.title : `new-post-${permalink}`;
+  const description = post.description ?  post.description.trim(): "#";
+  const image = post.image;
+  const body = post.body;
+
+  // ugly search and replace
+  // to be simplified
+  const postFilename = `${postSlug}-${title
+  .replace(new RegExp(' ', 'g'),"-")
+  .replace(new RegExp(':', 'g'),"")
+  .replace(new RegExp('\"', 'g'),"-")
+  .replace(new RegExp('\/', 'g'),"-")
+  .replace(new RegExp('&', 'g'),"-")
+  .replace(new RegExp("<br>", 'g'),"")
+  .replace(new RegExp(',', 'g'),"-")
+  .replace(new RegExp('\'', 'g'),"-")
+  .replace(new RegExp(':', 'g'),"-")
+  .replace(new RegExp('--', 'g'),"-")
+  .toLowerCase()}.md`;
+
+// compose final file string
+const newPost = `---
+title: "${title}"
+date: "${date}"
+image: "${image}"
+last_modified: "${modified}"
+description: |
+  "${description}"
+---
+
+${body}
+`;
+
+  // create file and write the formatted string to the file
+  Deno.writeTextFileSync(`${postFilename}`, newPost);  
+
+});
